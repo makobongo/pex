d170a78a1a3f5eebed118eac70abce1df4d36b82
steve obongo <steveobongo0@gmail.com>
2021-01-05 21:15:49 +0300

    added api end points

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+//Route::get('/24_volumes', function (){
+//    return['msg' => 'success'];
+//});
+Route::get('/24_volumes', 'App\Http\Controllers\MainController@volumes');
+Route::get('/ticker', 'App\Http\Controllers\MainController@ticker');
